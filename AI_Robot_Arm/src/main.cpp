ddb37815d4135940919995de397999e46107acf9
chickenshawarmasupremacy <izykhn16@gmail.com>
2026-08-03 13:14:50 +0530

    changes

--- AI_Robot_Arm/src/main.cpp
+++ AI_Robot_Arm/src/main.cpp
@@ -2,64 +2,43 @@
 Simple potentiometer to control the 
 */
 
+//
+
 
 
 #include <Arduino.h>
 #include <Servo.h>
 
-
-// put function declarations here:
-int myFunction(int, int);
-
 Servo myServo;
-
+Servo myServo2;
 
 const int inputPin = A0;
-const int outputPin = A3;
-/*
+const int outputPin1 = 3;
+const int outputPin2 = 2;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(inputPin, INPUT);
-  pinMode(outputPin, OUTPUT);
-  myServo.attach(outputPin);
+  pinMode(outputPin1, OUTPUT);
+  pinMode(outputPin2, OUTPUT);
+  myServo.attach(outputPin1);
+  myServo2.attach(outputPin2);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int input_value = analogRead(inputPin);
-  int final_val = (input_value/(91/4));
-  Serial.print("The value is: ");
-  Serial.println(final_val);
-  myServo.write(final_val);
-  delay(15);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-*/
-
-int pos = 0;   
-void  setup() {
-  myServo.attach(outputPin);  // attaches the servo on pin 9 to the servo object
-  Serial.begin(115200);
-  myServo.attach(outputPin);
-}
-
-void loop() {
-
-  for (int pos = 0; pos <= 180; pos += 1) {
-    Serial.println(pos);
-    myServo.write(pos);
+  
+  for (int i = 0; i < 180; i++){
+    myServo.write(i);
+    myServo2.write(180-i);
     delay(15);
+    Serial.println("Servo 1: " + String(myServo.read()));
+    Serial.println("Servo 2: " + String(myServo2.read()));
   }
-
-  for (int pos = 180; pos >= 0; pos -= 1) {
-    Serial.println(pos);
-    myServo.write(pos);
+  for (int i = 180; i > 0; i--){
+    myServo.write(i);
+    myServo2.write(180-i);
     delay(15);
-    
+    Serial.println("Servo 1: " + String(myServo.read()));
+    Serial.println("Servo 2: " + String(myServo2.read()));
   }
 }
