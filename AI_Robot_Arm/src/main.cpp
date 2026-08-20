a99de9bb3b2b4edc30d3430b80ba9681baa69ceb
chickenshawarmasupremacy <izykhn16@gmail.com>
2026-08-20 14:11:01 +0530

    yes ?

--- AI_Robot_Arm/src/main.cpp
+++ AI_Robot_Arm/src/main.cpp
@@ -1,49 +1,138 @@
 /* Code written by Izyan Khan
-Simple potentiometer to control the 
+Simple robot arm control
 */
 
-//
-
-
-
+<<<<<<< HEAD
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo myServo;
+
+//Servo declarations are here(Important to note: Servo is a data type)
+Servo clamper;
 Servo myServo2;
+Servo myServo3;
+Servo myServo4;
+Servo myServo5Link;
+Servo myServo6Link;
+Servo rotator;
 
-const int inputPin = 0;
-const int outputPin = 3;
-const int outputPin2 = 2;
+void moveServo(Servo &motor, int angle, int minAngle = 0, int maxAngle = 180) {
+  int currentAngle = motor.read();
+  int newAngle = currentAngle + angle;
+
+  // Ensure the new angle is within the specified range
+  if (newAngle < minAngle) {
+    newAngle = minAngle;
+  } else if (newAngle > maxAngle) {
+    newAngle = maxAngle;
+  }
+
+  motor.write(newAngle);
+}
 
 
-int pos = 0;   
 void  setup() {
-  myServo.attach(outputPin);  // attaches the servo on pin 9 to the servo object
-  myServo2.attach(outputPin2);
+  //Attaching all servo motors to their respective pins here
   Serial.begin(115200);
-  myServo.write(180);
+
+  // Attach all servos to their pins
+  clamper.attach(32);
+  clamper.write(0);
+  myServo2.attach(33);
   myServo2.write(0);
+  myServo3.attach(25);
+  myServo3.write(0);
+  myServo4.attach(26);
+  myServo4.write(0);
+  myServo5Link.attach(27);
+  myServo5Link.write(0);
+  myServo6Link.attach(14);
+  myServo6Link.write(180);
+  rotator.attach(12);
+
+  // Initial positions
+  clamper.write(0);
+  myServo2.write(90);
+  myServo3.write(90);
+  myServo4.write(90);
+  myServo5Link.write(90);
+  myServo6Link.write(90);
+  rotator.write(90);
 }
 
 void loop() {
-  char myControllerChar = '';
-  servoAngle = 0;
+  if (Serial.available() > 0) {
+    char controller = Serial.read();
 
-    myControllerChar = Serial.read();
-    if (myControllerChar == 'a') {
-      
-      servoAngle += 1;
-    }
-    else if (myControllerChar == 'd') {
-      myServo.write(servoAngle);
-      myServo2.write(180 - servoAngle);
-      servoAngle -= 1;
-    }
-    else if (myControllerChar == '')
-      Serial.println("Please enter (a or d): ");
-    else {
-      Serial.println("Please enter a valid input (a or d): ");
+    switch (controller) {
+      case ' ':
+        if (clamper.read() == 0) {
+        moveServo(clamper, 90, 0, 90); // Open the clamper
+        } else {
+          moveServo(clamper, -90, 0, 90); // Close the clamper
+        }
+        break;
+
+      case 'w':
+        moveServo(myServo2, 15);
+        break;
+
+      case 's':
+        moveServo(myServo2, -15);
+        break;
+
+      case 'a':
+        moveServo(myServo3, 15);
+        break;
+
+      case 'd':
+        moveServo(myServo3, -15);
+        break;
+
+      case 'q':
+        moveServo(myServo4, 15);
+        break;
+
+      case 'e':
+        moveServo(myServo4, -15);
+        break;
+//Not using the moveServo command here to fix the issue of two seros being connected to drive the same joint.
+      case 'z':
+        if (myServo5Link.read() < 166) {
+          int tempAngle = myServo5Link.read() + 15;
+          myServo5Link.write(tempAngle);
+          myServo6Link.write(180 - tempAngle);
+        }
+        else {
+          myServo5Link.write(180);
+          myServo6Link.write(0);
+        }
+        break;
+      case 'v':
+        if (myServo5Link.read() > 15) {
+          int tempAngle = myServo5Link.read() - 15;
+          myServo5Link.write(tempAngle);
+          myServo6Link.write(180 - tempAngle);
+        }
+        else {
+          myServo5Link.write(0);
+          myServo6Link.write(180);
+        }
+        break;
+
+      case 'r':
+        moveServo(rotator, 15);
+        break;
+
+      case 'f':
+        moveServo(rotator, -15);
+        break;
+
+      default:
+      Serial.println("Invalid command received");
+        break;
+
     }
   }
-
+}
+>>>>>>> 5c21d05 (Semi final commit, wifi capabilities need to be added)
